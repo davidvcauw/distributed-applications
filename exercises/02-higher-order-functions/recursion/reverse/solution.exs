463620126b227a9c9bf7abd1e1d887493138ec31
Wannes Fransen <wannes.fransen@ucll.be>
2019-09-25 11:30:07 +0200

    requested file changes

--- exercises/02-higher-order-functions/recursion/reverse/solution.exs
+++ exercises/02-higher-order-functions/recursion/reverse/solution.exs
@@ -1,10 +1,4 @@
 defmodule Exercise do
   def reverse([]), do: []
-  def reverse([x|xs]), do: reverse(xs) ++ [x]
-end
-
-defmodule Exercise2 do
-  def reverse(arg), do: preverse(arg, [])
-  def preverse([], acc), do: acc |> List.flatten()
-  def preverse([x | xs], acc), do: preverse(xs, [x | acc])
+  def reverse([x | xs]), do: reverse(xs) ++ [x]
 end
