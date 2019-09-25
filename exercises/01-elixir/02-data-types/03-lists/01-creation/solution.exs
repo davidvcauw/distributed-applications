0422aa107bf44c7d91e0de28840b544ea86d5d89
Wannes Fransen <wannes.fransen@ucll.be>
2019-09-25 11:32:16 +0200

    requested file changes

--- exercises/01-elixir/02-data-types/03-lists/01-creation/solution.exs
+++ exercises/01-elixir/02-data-types/03-lists/01-creation/solution.exs
@@ -2,9 +2,3 @@ defmodule Util do
   def range(a, b) when is_number(a) and a <= b, do: [a | range(a + 1, b)]
   def range(a, _) when is_number(a), do: []
 end
-
-defmodule Util2 do
-  def range(a, b), do: calcrange(a, b, [])
-  defp calcrange(a, b, acc) when b < a, do: acc
-  defp calcrange(a, b, acc), do: calcrange(a, b - 1, [b | acc])
-end
