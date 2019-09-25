42a0ba3a905e3f57b7e91be2bc13b928da629a86
WannesFransen1994 <wannes.fransen@gmail.com>
2019-09-25 10:07:16 +0200

    alternative, more performant solution
    defmodule Benchmark do
      def measure(function) do
        function
        |> :timer.tc()
        |> elem(0)
        |> Kernel./(1_000_000)
      end
    end
    
    defmodule Random do
      def random_array(length, list \\ [])
      def random_array(0, list), do: list
      def random_array(length, list), do: random_array(length - 1, [random_number() | list])
      def random_number(), do: :rand.uniform(1000)
    end
    
    n = Random.random_array(2_000_000)
    t1 = Benchmark.measure(fn -> Exercise.reverse(n) end)
    t2 = Benchmark.measure(fn -> Exercise2.reverse(n) end)
    
    
    t1 is op mijn pc ong 98 secs
    t2 is 0.015s
    
    Ze zouden bij deze les de theorie al moeten kennen waarom prependen performanter is.

--- exercises/02-higher-order-functions/recursion/reverse/solution.exs
+++ exercises/02-higher-order-functions/recursion/reverse/solution.exs
@@ -2,3 +2,9 @@ defmodule Exercise do
   def reverse([]), do: []
   def reverse([x|xs]), do: reverse(xs) ++ [x]
 end
+
+defmodule Exercise2 do
+  def reverse(arg), do: preverse(arg, [])
+  def preverse([], acc), do: acc |> List.flatten()
+  def preverse([x | xs], acc), do: preverse(xs, [x | acc])
+end
